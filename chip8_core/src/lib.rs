f96c169324bc1954a56d9ceaa5181daf9e87e457
Tomoyuki Murakami <tomoyukim@outlook.com>
2024-10-18 23:41:50 +0900

    implemented to the end of chapter4.

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -3,8 +3,31 @@ pub const SCREEN_HEIGHT: usize = 32;
 
 const RAM_SIZE: usize = 4096; // not defined in the spec but 4kb is de fact standard
 const NUM_REGS: usize = 16;
+// STACK_SIZE is not defined in the spec but 16 is the de-fact standard among emu develolpers.
+// The std libraries are not work for WebAssembly! So, VecDeque is not appropriate here.
 const STACK_SIZE: usize = 16;
 const NUM_KEYS: usize = 16;
+
+const FONTSET_SIZE: usize = 80;
+
+const FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
 
 pub struct Emu {
     pc: u16,
@@ -15,6 +38,8 @@ pub struct Emu {
     sp: u16,
     stack: [u16; STACK_SIZE],
     keys: [bool; NUM_KEYS],
+    // There are 2 timers; delay timer and sound timer.
+    // The sound timer is the only way to play a sound.
     dt: u8,
     st: u8,
 }
@@ -27,10 +52,10 @@ const START_ADDR: u16 = 0x200;
 
 impl Emu {
     pub fn new() -> Self {
-        Self {
+        let mut new_emu = Self {
             pc: START_ADDR,
             ram: [0; RAM_SIZE],
-            screen: [false, SCREEN_WIDTH * SCREEN_HEIGHT],
+            screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
             v_reg: [0; NUM_REGS],
             i_reg: 0,
             sp: 0,
@@ -38,11 +63,65 @@ impl Emu {
             keys: [false; NUM_KEYS],
             dt: 0,
             st: 0,
+        };
+
+        new_emu.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+
+        new_emu
+    }
+
+    pub fn reset(&mut self) {
+        self.pc = START_ADDR;
+        self.ram = [0; RAM_SIZE];
+        self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+        self.v_reg = [0; NUM_REGS];
+        self.i_reg = 0;
+        self.sp = 0;
+        self.stack = [0; STACK_SIZE];
+        self.keys = [false; NUM_KEYS];
+        self.dt = 0;
+        self.st = 0;
+        self.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+    }
+
+    fn push(&mut self, val: u16) {
+        self.stack[self.sp as usize] = val;
+        self.sp += 1;
+    }
+
+    fn pop(&mut self) -> u16 {
+        // It can be panic when the operation result in the underflow.
+        // You can add code to handle the case but nothing here is intentional by the book.
+        self.sp -= 1;
+        self.stack[self.sp as usize]
+    }
+
+    pub fn tick(&mut self) {
+        // Fetch
+        let op = self.fetch();
+        // Decode
+        // Execute
+    }
+
+    fn fetch(&mut self) -> u16 {
+        // TODO
+        let higher_byte = self.ram[self.pc as usize] as u16;
+        let lower_byte = self.ram[(self.pc + 1) as usize] as u16;
+        let op = (higher_byte << 8) | lower_byte;
+        self.pc += 2;
+        op
+    }
+
+    pub fn tick_timers(&mut self) {
+        if self.dt > 0 {
+            self.dt -= 1;
+        }
+
+        if self.st > 0 {
+            if self.st == 1 {
+                // TODO: BEEP
+            }
+            self.st -= 1;
         }
     }
 }
-
-// stack num is not defined in the spec but 16 is the de-fact standard among emu develolpers
-// std libraries are not work for WebAssembly! VecDeque is not appropriate here.
-
-// 2 timers; delay timer and sound timer. the sound timer is the only way to play a sound.
